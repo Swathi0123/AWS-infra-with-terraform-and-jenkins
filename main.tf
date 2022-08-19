70f6bd637d84f3e5a140275a42c32fd15012e832
Swathi0123 <81679244+Swathi0123@users.noreply.github.com>
2022-08-19 13:55:23 +0530

    main

---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+region = var.aws_region
+access_key = "AKIAZ2I4M5UPXVOU33Py"
+secret_key = "CWM6ffSWdEqY9OmaXNHm0216ZbG1yehkkxzbW0vN"
+endpoints {
+sts = "https://sts.us-east-1.amazonaws.com"
+}
+assume_role {
+role_arn = "arn:aws:iam::674906303775:role/my_ec2_terraform_role"
+session_name = "terraform_session_name"
+}
+}
+
+#Create security group with firewall rules
+resource "aws_security_group" "my_security_group" {
+  name        = var.security_group
+  description = "security group for Ec2 instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
+  }
+}
+
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
+  }
+}
+
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
+  }
+}
