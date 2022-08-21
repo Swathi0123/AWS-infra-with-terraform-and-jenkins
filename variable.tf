89bd952e97f4ed2a295ac8b3b9123a83c5fe4866
Swathi0123 <81679244+Swathi0123@users.noreply.github.com>
2022-08-21 10:49:45 +0530

    updated sg

--- variable.tf
+++ variable.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "demo_terraform"
+  default     = "demo123_terraform"
 }
 
 variable "tag_name" {
